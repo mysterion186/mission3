8ef13b3d16d8fe7f505cee5eb2a374cc4e2a05bc
Mysterion186 <kowsikan@hotmail.fr>
2023-11-26 15:07:19 +0100

    add urls conf

---
+++ backend/cards/urls.py
@@ -0,0 +1,12 @@
+"""urls conf for the cards app."""
+from django.urls import path
+
+from . import views
+
+app_name = 'cards'
+
+urlpatterns = [
+    path("flashcard/list", view=views.FlashcardListView.as_view(), name="flashcard_list"),
+    path("deck/list", view=views.DeckListView.as_view(), name="deck_list"),
+    path("theme/list", view=views.ThemeListView.as_view(), name="theme_list")
+]
