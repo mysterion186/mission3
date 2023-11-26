07b77c8f02605be1ecca0fbddbf1613a35bb0f0f
Mysterion186 <kowsikan@hotmail.fr>
2023-11-26 18:20:53 +0100

    update the get_deck_name method (handle orderedDict) When I try to create several Flashcard I get an error that the obj object is an OrderedDict and not in instance of Deck

---
+++ backend/cards/serializers.py
@@ -0,0 +1,42 @@
+"""Serializer related to the cards application."""
+from rest_framework import serializers
+
+from accounts.models import MyUser
+from .models import Flashcard, Deck, Theme
+
+class ThemeSerializer(serializers.ModelSerializer):
+    """Serializer for the Theme model."""
+    class Meta:
+        """Default meta class."""
+        model = Theme
+        fields = ["id", "theme"]
+
+class DeckSerializer(serializers.ModelSerializer):
+    """Serializer for the Deck model."""
+    user_email = serializers.SerializerMethodField()
+    class Meta:
+        """Default meta class."""
+        model = Deck
+        fields = ["id", "owner", "user_email", "theme", "name"]
+
+    def get_user_email(self, obj):
+        """Return the user's email."""
+        user = obj.owner
+        return user.email
+
+class FlashcardSerializer(serializers.ModelSerializer):
+    """Serializer for the Flashcard model."""
+    deck_name = serializers.SerializerMethodField()
+    class Meta:
+        """Default meta class."""
+        model = Flashcard
+        fields = ["deck", "deck_name", "question", "answer", "position"]
+
+    def get_deck_name(self, obj):
+        """Return the name of the Deck."""
+        if isinstance(obj, dict):
+            deck = obj.get("deck")
+            if deck:
+                return deck.name
+        deck = obj.deck
+        return deck.name
