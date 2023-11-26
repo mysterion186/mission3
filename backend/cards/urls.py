8ab2cd360fc4959cfc89a92dae4ee6084503bd77
Mysterion186 <kowsikan@hotmail.fr>
2023-11-26 15:36:02 +0100

    views/urls for listing cards models + create a deck

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -8,5 +8,6 @@ app_name = 'cards'
 urlpatterns = [
     path("flashcard/list", view=views.FlashcardListView.as_view(), name="flashcard_list"),
     path("deck/list", view=views.DeckListView.as_view(), name="deck_list"),
-    path("theme/list", view=views.ThemeListView.as_view(), name="theme_list")
+    path("deck", view=views.DeckCreateView.as_view(), name="deck_create"),
+    path("theme/list", view=views.ThemeListView.as_view(), name="theme_list"),
 ]
--- backend/cards/views.py
+++ backend/cards/views.py
@@ -1,3 +1,34 @@
+"""Views for the cards app."""
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveAPIView,
+    ListAPIView
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from . import models
+from . import serialiazers
+class FlashcardListView(ListAPIView):
+    """View for listing all existing Flashcards"""
+    serializer_class = serialiazers.FlashcardSerializer
+    queryset = models.Flashcard.objects.all()
+    permission_classes = [AllowAny]
+
+class DeckListView(ListAPIView):
+    """View for listing all Decks."""
+    serializer_class = serialiazers.DeckSerializer
+    queryset = models.Deck.objects.all()
+    permission_classes = [AllowAny]
+
+class ThemeListView(ListAPIView):
+    """View for listing all the Theme."""
+    serializer_class = serialiazers.ThemeSerializer
+    queryset = models.Theme.objects.all()
+    permission_classes = [AllowAny]
+
+class DeckCreateView(CreateAPIView):
+    """Create a Deck."""
+    serializer_class = serialiazers.DeckSerializer
+    permission_classes = [IsAuthenticated]
