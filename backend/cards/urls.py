e0fc256be61063bc1ed800f576129aa43089960d
Mysterion186 <kowsikan@hotmail.fr>
2023-11-26 17:50:44 +0100

    update path on the url file

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -7,6 +7,7 @@ app_name = 'cards'
 
 urlpatterns = [
     path("flashcard/list", view=views.FlashcardListView.as_view(), name="flashcard_list"),
+    path("flashcard", view=views.FlashcardCreateView.as_view(), name="flashcard_create"),
     path("deck/list", view=views.DeckListView.as_view(), name="deck_list"),
     path("deck", view=views.DeckCreateView.as_view(), name="deck_create"),
     path("theme/list", view=views.ThemeListView.as_view(), name="theme_list"),
