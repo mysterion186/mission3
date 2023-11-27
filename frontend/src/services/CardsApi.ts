36fcacecf0564de0be1dcfa4f06d5d5638876fbc
Mysterion186 <kowsikan@hotmail.fr>
2023-11-27 21:02:42 +0100

    update method for the cards api

---
+++ frontend/src/services/CardsApi.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { type_headers } from "../types/api.types";
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL
+});
+
+export default {
+    async call(method: string, ressources: string, data:object | null = null, token: string | null = null ){
+        const headers: type_headers = {
+            "Content-type": "application/json"
+        };
+
+        if (token !== null){
+            headers.authorization = "Bearer " + token;
+        }
+        return instance({
+            method,
+            headers: headers,
+            url: ressources,
+            data
+        }).then((res) => {
+            return {status: res.status, data: res.data};
+        }).catch((err) => {
+            return {status: err.response.status, data: err.response.data};
+        })
+    },
+
+    // returns all the decks that are in the app
+    async getAllDecks(){
+        return this.call("get", "/cards/deck/list");
+    },
+
+    // returns all flashcard for a specific Deck
+    async getQuiz(id: number){
+        return this.call("get", `/cards/deck/${String(id)}`)
+    },
+
+}
