f3542ba7280946d3c83e02fbb429c77563ab1ba2
Mysterion186 <kowsikan@hotmail.fr>
2023-11-28 19:37:06 +0100

    method for saving/retrieving user's id in local storage

--- frontend/src/services/AuthStorage.ts
+++ frontend/src/services/AuthStorage.ts
@@ -9,5 +9,11 @@ export default{
     },
     getJWTToken():string | null{
         return window.localStorage.getItem("token");
+    },
+    saveUserId(id: string):void {
+        window.localStorage.setItem("id", id);
+    },
+    getUserId(): string | null{
+        return window.localStorage.getItem("id");
     }
 }
